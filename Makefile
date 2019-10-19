cdd87d81ef804660bf303f0df9404a50ca2b1c40
HeZehao <hezh29@mail2.sysu.edu.cn>
2019-10-19 04:04:13 -0400

    a

--- Makefile
+++ Makefile
@@ -1,25 +1,3 @@
-# CC := g++
-# FLAGS := -std=c++2a -w -g -pthread
-# INC_DIR := include
-# SRC_DIR := src
-# BUILD_DIR := build
-# BIN_DIR := bin
-# TEST_DIR := test
-# INCLUDE := -I./$(INC_DIR) 
-# TEST_LIBRARY := -lgtest -lgtest_main
-
-# DEST := test
-# SOURCE_FILES=$(shell find $(SRC_DIR) -name '*.cpp')
-# OBJS=$(patsubst $(SOURCE_FILES)/%.cpp,$(BUILD_DIR)/%.o,$(SOURCE_FILES))
-
-# $(DEST): $(OBJS)
-# 	@mkdir -p $(BIN_DIR)
-# 	$(CC) $(FLAGS) $(INCLUDE) $^ -o $@ $(TEST_LIBRARY)
-
-# $(BUILD_DIR)/%.o: $(SRC_DIR)/%.cpp
-# 	@mkdir -p $(BUILD_DIR)
-# 	$(CC) $(FLAGS) $(INCLUDE) -c -o $@ $<
-
 CC := g++
 FLAGS := -std=c++11 -w
 INC_DIR := include
@@ -29,7 +7,7 @@ BIN_DIR := bin
 DATA_DIR := data
 INCLUDE := -I./$(INC_DIR)
 
-$(BIN_DIR)/main: $(BUILD_DIR)/User.o $(BUILD_DIR)/Date.o $(BUILD_DIR)/Meeting.o $(BUILD_DIR)/Storage.o $(BUILD_DIR)/AgendaService.o $(BUILD_DIR)/main.o
+$(BIN_DIR)/main: $(BUILD_DIR)/User.o $(BUILD_DIR)/Date.o $(BUILD_DIR)/Meeting.o $(BUILD_DIR)/Storage.o $(BUILD_DIR)/main.o
 	@mkdir -p $(BIN_DIR)
 	$(CC) $(FLAGS) $(INCLUDE) $^ -o $@ 
 
@@ -43,10 +21,6 @@ $(BUILD_DIR)/Meeting.o: $(SRC_DIR)/Meeting.cpp $(INC_DIR)/Meeting.hpp $(INC_DIR)
 
 $(BUILD_DIR)/Storage.o: $(SRC_DIR)/Storage.cpp $(INC_DIR)/Storage.hpp $(INC_DIR)/Path.hpp $(INC_DIR)/User.hpp $(INC_DIR)/Meeting.hpp
 	@mkdir -p $(BUILD_DIR) $(DATA_DIR)
-	$(CC) $(FLAGS) $(INCLUDE) -c -o $@ $<	
-
-$(BUILD_DIR)/AgendaService.o: $(SRC_DIR)/AgendaService.cpp $(INC_DIR)/AgendaService.hpp $(INC_DIR)/Storage.hpp
-	@mkdir -p $(BUILD_DIR)
 	$(CC) $(FLAGS) $(INCLUDE) -c -o $@ $<	
 
 $(BUILD_DIR)/%.o: $(SRC_DIR)/%.cpp $(INC_DIR)/%.hpp
--- main/main.cpp
+++ main/main.cpp
@@ -1,4 +1,3 @@
-#include"AgendaService.hpp"
 #include"Storage.hpp"
 #include<iostream>
 
@@ -6,38 +5,31 @@
 using namespace std;
 
 int main(){
-    // auto s = Storage::getInstance();
 
-    // for(auto i:s->queryMeeting([](const Meeting & m)->bool{return true;})){
-    //     cout<<i.getTitle()<<endl;
-    //     for(auto j:  i.getParticipator()){
-    //         cout << j<<endl;
+    auto storage = Storage::getInstance();
+    cout << storage->updateMeeting([](const Meeting a) -> bool{if(a.getSponsor()[0] == 'G')return true;return false;}, [](Meeting &a) -> void{a.setTitle("p");});
+
+    // for(auto i : us){
+    // cout << i.getSponsor() << endl;
+
+    //     for(auto p : i.getParticipator()){
+    //         cout << p << endl;
     //     }
-    //     cout<<endl;
+    // cout << Date::dateToString(i.getStartDate()) << endl;
+
+    // cout << Date::dateToString(i.getEndDate()) << endl;
+    // cout << i.getTitle() << endl<<endl;
     // }
-    // int k;
-    // cin>>k;
 
-    AgendaService m_service;
-
-    m_service.userRegister("na", "pb", "ec", "hd");
-    m_service.userRegister("nb", "pb", "ec", "hd");
-    m_service.userRegister("nc", "pc", "ec", "hd");
-    m_service.userRegister("nd", "pc", "ec", "hd");
-    m_service.userRegister("ne", "pc", "ec", "hd");
-    m_service.userRegister("nf", "pc", "ec", "hd");
-
-
-    m_service.createMeeting("na", "tb", "2016-08-01/00:00", "2016-08-01/12:00", {"nb", "na", "nd"});
-    m_service.createMeeting("na", "tf", "2026-08-01/00:00", "2026-08-01/12:00", {"nb", "na", "nd", "nc"});
-    m_service.createMeeting("na", "tg", "2036-08-01/00:00", "2036-08-01/12:00", {"nb", "na", "nd"});
-
-    m_service.createMeeting("nc", "tc", "2056-08-01/11:00", "2056-09-01/12:00", { "na"});
-
-    m_service.createMeeting("nc", "tc", "2046-08-01/11:00", "2046-09-01/12:00", { "na"});
+    Meeting m;
     
-
-
+    // m.setSponsor("Geralt of Rivia");
+    // m.setParticipator({"Naked Snake","Lara Croft"});
+    // m.setStartDate(Date("2016-07-10/15:00"));
+    // m.setEndDate({"2016-07-10/18:00"});
+    // m.setTitle("Want a few rounds of Gwent?");
+    //  storage->createMeeting(m);
+    //  cout << storage->sync();
 
     return 0;
 }
